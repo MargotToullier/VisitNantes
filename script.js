6868510404094c9a6e7ee87f2ee354d97bd7be6c
Clemence Pirault <clepirault@gmail.com>
2021-03-25 10:41:09 +0100

    adding JavaScript in form send button

---
+++ script.js
@@ -0,0 +1,9 @@
+const formSent = document.querySelector('.formButton');
+formSent.addEventListener('click', function(){
+    alert("Nous vous remercions pour votre message !");
+})
+
+const newsletterSent = document.querySelector('.newsletterButton');
+newsletterSent.addEventListener('click', function(){
+    alert("Votre inscription est prise en compte ! ");
+})
